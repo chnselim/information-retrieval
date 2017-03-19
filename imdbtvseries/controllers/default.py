e503b099cb6a08931e2301a72afbe924f78964f5
Cihan Selim <chnselim@outlook.com>
2017-03-20 00:36:36 +0300

    solr

--- imdbtvseries/controllers/default.py
+++ imdbtvseries/controllers/default.py
@@ -9,10 +9,33 @@
 #########################################################################
 
 def index():
-    import json
-    data = json.loads('[{"name": "The Walking Dead", "image": "https://images-na.ssl-images-amazon.com/images/M/MV5BMTczMDk1NDYyMV5BMl5BanBnXkFtZTgwNjE1NDU4MDI@._V1_UX182_CR0,0,182,268_AL_.jpg", "summary": "Sheriff Deputy Rick Grimes wakes up from a coma, to learn the world is in ruins, and must lead a group of survivors to stay alive.", "genre": ["Drama", "Horror", "Thriller"], "rate": "9.7", "year": "2010", "vote": "680,928"}]')
+
     return locals()
 
+def search():
+    import json, urllib
+    # corename = "imdbSeries"
+    # data = json.loads('[{"name": "The Walking Dead", "image": "https://images-na.ssl-images-amazon.com/images/M/MV5BMTczMDk1NDYyMV5BMl5BanBnXkFtZTgwNjE1NDU4MDI@._V1_UX182_CR0,0,182,268_AL_.jpg", "summary": "Sheriff Deputy Rick Grimes wakes up from a coma, to learn the world is in ruins, and must lead a group of survivors to stay alive.", "genre": ["Drama", "Horror", "Thriller"], "rate": "9.7", "year": "2010", "vote": "680,928"}]')
+    # search_url = "http://localhost:8983/solr/imdbSeries/select?indent=on&q=*:*&wt=json"
+    # search_resp = urllib.urlopen(search_url)
+    # data = json.loads(search_resp.read())
+
+    # search_query = request.vars.searchQuery.replace(' ', '+')
+    # if not request.vars.prevQuery:
+    #     request.vars.prevQuery = request.vars.searchQuery
+    # if request.vars.prevQuery != request.vars.searchQuery:
+    #     search_query = request.vars.prevQuery + "+AND+" + search_query
+    # page = request.args(0, cast=int) or 0
+    # category = request.vars.cat or "*"
+    # date = request.vars.date or "*"
+    # search_result_url = "http://localhost:8983/solr/Mashable/select?q=category:{0}+AND+date:{3}+AND+{1}&start={2}&wt=json&indent=true&facet=true&facet.field=category".format(category, search_query, (page-1)*10, date)
+    # # return search_result_url
+    # search_response = urllib.urlopen(search_result_url)
+    # search_result = json.loads(search_response.read())
+    return locals()
+
+def details():
+    return locals()
 
 def user():
     """
