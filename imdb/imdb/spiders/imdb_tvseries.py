7deeb3aabbf9c0bccfd94b57d2fd927ef9ce8727
Cihan Selim <chnselim@outlook.com>
2017-05-20 12:49:14 +0300

    details page

--- imdb/imdb/spiders/imdb_tvseries.py
+++ imdb/imdb/spiders/imdb_tvseries.py
@@ -5,30 +5,30 @@ class ImdbTvseriesSpider(scrapy.Spider):
     name = "imdb_tvseries"
     #http://www.imdb.com/search/title?title_type=tv_series&page=1
     start_urls = []
-    for c in range(1,3):
+    for c in range(1,60):
         start_urls.append('http://www.imdb.com/search/title?title_type=tv_series&page={}'.format(c))
 
-    def parse(self, response):
-        links = response.css("h3.lister-item-header a::attr(href)").extract()
-        for l in links[:5]:
-            l = response.urljoin(l)
-            yield scrapy.Request(l, callback=self.parse_doc)
+        def parse(self, response):
+            links = response.css("h3.lister-item-header a::attr(href)").extract()
+            for l in links[:50]:
+                l = response.urljoin(l)
+                yield scrapy.Request(l, callback=self.parse_doc)
 
-    def parse_doc(self, response):
-        name = response.css("div.title_wrapper h1::text")[0].extract().strip()
-        year = response.css("div.title_wrapper div.subtext a::text").extract_first()
-        rate = response.css("div.ratings-bar div strong::text")[0].extract()
-        image = response.css("div.poster a img::attr(src)").extract_first()
-        genre = response.css("div.title_wrapper div.subtext a span::text").extract()
-        vote = response.css("div.imdbRating a span.small::text").extract_first()
-        summary = response.css("div.summary_text::text").extract_first().strip()
+        def parse_doc(self, response):
+            name = response.css("div.title_wrapper h1::text")[0].extract().strip()
+            year = response.css("div.title_wrapper div.subtext a::text").extract_first()
+            rate = response.css("div.ratings-bar div strong::text")[0].extract()
+            image = response.css("div.poster a img::attr(src)").extract_first()
+            genre = response.css("div.title_wrapper div.subtext a span::text").extract()
+            vote = response.css("div.imdbRating a span.small::text").extract_first()
+            summary = response.css("div.summary_text::text").extract_first().strip()
 
-        yield {
-              "name": name,
-              "year": year[11:15],
-              "rate": rate,
-              "genre": genre,
-              "image": image,
-              "vote": vote,
-              "summary": summary
-        }
+            yield {
+                  "name": name,
+                  "year": year[11:15],
+                  "rate": rate,
+                  "genre": genre,
+                  "image": image,
+                  "vote": vote,
+                  "summary": summary
+            }
--- imdbtvseries/controllers/default.py
+++ imdbtvseries/controllers/default.py
@@ -1,75 +1,42 @@
 # -*- coding: utf-8 -*-
-# this file is released under public domain and you can use without limitations
-
-#########################################################################
-## This is a sample controller
-## - index is the default action of any application
-## - user is required for authentication and authorization
-## - download is for downloading files uploaded in the db (does streaming)
-#########################################################################
 
 def index():
+    """ removing item from solr """
+    #http://localhost:8983/solr/imdb/update?stream.body=%3Cdelete%3E%3Cquery%3Eid:03109f46-3938-402f-94e9-4ae006466525%3C/query%3E%3C/delete%3E&commit=true
+    """ search field is exist in solr """
+    #-field:[* *]
 
     return locals()
 
 def search():
     import json, urllib
-    # corename = "imdbSeries"
-    # data = json.loads('[{"name": "The Walking Dead", "image": "https://images-na.ssl-images-amazon.com/images/M/MV5BMTczMDk1NDYyMV5BMl5BanBnXkFtZTgwNjE1NDU4MDI@._V1_UX182_CR0,0,182,268_AL_.jpg", "summary": "Sheriff Deputy Rick Grimes wakes up from a coma, to learn the world is in ruins, and must lead a group of survivors to stay alive.", "genre": ["Drama", "Horror", "Thriller"], "rate": "9.7", "year": "2010", "vote": "680,928"}]')
-    # search_url = "http://localhost:8983/solr/imdbSeries/select?indent=on&q=*:*&wt=json"
-    # search_resp = urllib.urlopen(search_url)
-    # data = json.loads(search_resp.read())
+    search_query = request.vars.q.replace(' ', '+')
 
-    # search_query = request.vars.searchQuery.replace(' ', '+')
-    # if not request.vars.prevQuery:
-    #     request.vars.prevQuery = request.vars.searchQuery
-    # if request.vars.prevQuery != request.vars.searchQuery:
-    #     search_query = request.vars.prevQuery + "+AND+" + search_query
-    # page = request.args(0, cast=int) or 0
-    # category = request.vars.cat or "*"
-    # date = request.vars.date or "*"
-    # search_result_url = "http://localhost:8983/solr/Mashable/select?q=category:{0}+AND+date:{3}+AND+{1}&start={2}&wt=json&indent=true&facet=true&facet.field=category".format(category, search_query, (page-1)*10, date)
-    # # return search_result_url
-    # search_response = urllib.urlopen(search_result_url)
-    # search_result = json.loads(search_response.read())
+    genre = request.vars.catQuery or "*"
+    year = request.vars.yearQuery or "*"
+    rate = request.vars.rateQuery or "*"
+
+    page = request.args(0) or 1
+    search_result_url = "http://127.0.0.1:8983/solr/imdb/select?indent=on&q={1}&fq=genre:{0}&fq=year:{3}&fq=rate:{4}&rows=12&facet=true&facet.field=genre&facet.field=year&start={2}&wt=json".format(genre, search_query, (int(page)-1)*12, year, rate)
+    search_response = urllib.urlopen(search_result_url)
+    search_result = json.loads(search_response.read())
     return locals()
 
 def details():
+    import urllib
+    import json
+    id = request.args(0)
+    search_result_url = "http://127.0.0.1:8983/solr/imdb/select?indent=on&q={}&df=id&wt=json".format(id)
+    search_response = urllib.urlopen(search_result_url)
+    search_result = json.loads(search_response.read())
     return locals()
 
 def user():
-    """
-    exposes:
-    http://..../[app]/default/user/login
-    http://..../[app]/default/user/logout
-    http://..../[app]/default/user/register
-    http://..../[app]/default/user/profile
-    http://..../[app]/default/user/retrieve_password
-    http://..../[app]/default/user/change_password
-    http://..../[app]/default/user/manage_users (requires membership in
-    http://..../[app]/default/user/bulk_register
-    use @auth.requires_login()
-        @auth.requires_membership('group name')
-        @auth.requires_permission('read','table name',record_id)
-    to decorate functions that need access control
-    """
     return dict(form=auth())
-
 
 @cache.action()
 def download():
-    """
-    allows downloading of uploaded files
-    http://..../[app]/default/download/[filename]
-    """
     return response.download(request, db)
 
-
 def call():
-    """
-    exposes services. for example:
-    http://..../[app]/default/call/jsonrpc
-    decorate with @services.jsonrpc the functions to expose
-    supports xml, json, xmlrpc, jsonrpc, amfrpc, rss, csv
-    """
     return service()
